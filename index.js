c51d8671a37572510cdcf33ab68dc2b997e03a6f
student <abdullahihamdiabass@gmail.com>
2021-03-21 21:06:54 +0300

    javascript2

---
+++ index.js
@@ -0,0 +1,19 @@
+function getGrade(marks){
+    var marks=80;
+    getGrade.mark("A");
+}console.log("A");
+    
+    var marks=70-79;
+ 
+    { console.log("B");
+    var marks=60-69;
+
+    { console.log("C");
+
+    var marks=50-59;
+}   console.log("D");
+
+    var marks=0-49;
+    console.log("E")
+    
+    }console.log("F");
